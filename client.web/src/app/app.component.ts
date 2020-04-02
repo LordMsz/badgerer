1bc203c38fd2e6bd3a13f2a8400e42f6e62bab82
LordMsz <Mr.MarekSmolik@gmail.com>
2020-04-02 21:55:12 +0200

    Simple API call refresh Add angular Material

--- client.web/src/app/app.component.ts
+++ client.web/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -12,12 +12,22 @@ interface IWeatherResponse {
 @Component({
   selector: 'badgerer-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   public watherTest$: Observable<IWeatherResponse[]>;
 
   public constructor(private httpClient: HttpClient) {
     this.watherTest$ = httpClient.get<IWeatherResponse[]>('https://localhost:5001/WeatherForecast');
+
+    //TODO: a demo with onpush on root and setInterval and other stuff?
+    // window.setInterval(() => {
+    //   this.watherTest$ = this.httpClient.get<IWeatherResponse[]>('https://localhost:5001/WeatherForecast');
+    // }, 500);
+  }
+
+  public onReloadClick(): void {
+    this.watherTest$ = this.httpClient.get<IWeatherResponse[]>('https://localhost:5001/WeatherForecast');
   }
 }
--- client.web/src/app/app.module.ts
+++ client.web/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
