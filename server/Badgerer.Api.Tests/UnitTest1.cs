f136468786873a4b7b43b4bc57442b10a03c6b32
LordMsz <Mr.MarekSmolik@gmail.com>
2022-05-26 01:52:04 +0200

    Fix failing build for SSDT project and add dummy test

---
+++ server/Badgerer.Api.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Badgerer.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is temporary test");
+        }
+    }
+}
