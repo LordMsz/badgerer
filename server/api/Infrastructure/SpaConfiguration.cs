99a21e03204f0de7d4b781d2253f4f3488224179
LordMsz <Mr.MarekSmolik@gmail.com>
2022-05-28 02:17:43 +0200

    Update to Angular 11 + fix Spa .NET dev start

--- server/api/Infrastructure/SpaConfiguration.cs
+++ server/api/Infrastructure/SpaConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class SpaSettings
     {
-        public string StaticFilesRootPath { get; set; } = "ClientApp";
+        public string StaticFilesRootPath { get; set; } = "ClientApp/Badgerer";
         public string SourcePath { get; set; } = "../../client.web";
     }
 }
